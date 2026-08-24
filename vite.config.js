bf821792e0d3928ff5b0cd00156f21b801816091
Ryan Hoffman <ryanhoffman4@gmail.com>
2026-08-23 23:56:41 -0700

    Build: add background styling

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(_dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     }
   }
 });
