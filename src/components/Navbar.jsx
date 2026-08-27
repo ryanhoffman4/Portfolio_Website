5d665597696645cfe95224fd1e100002011d43e1
Ryan Hoffman <ryanhoffman4@gmail.com>
2026-08-27 14:41:17 -0700

    Build: add footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,6 @@ const navItems = [
     {name: "About", href: "#about"},
     {name: "Skills", href: "#skills"},
     {name: "Projects", href: "#projects"},
-    {name: "Contact", href: "#contact"},
 ];
 
 export const Navbar = () => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
-import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-forground overflow-x-hidden">
@@ -24,10 +24,10 @@ export const Home = () => {
             <AboutSection />
             <SkillsSection />
             <ProjectsSection />
-            <ContactSection />
         </main>
 
         {/* Footer */}
+        <Footer />
 
     </div>
 }
