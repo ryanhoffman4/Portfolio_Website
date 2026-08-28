fbac0e5f242077d4e55cd4859da47f3bb7499681
Ryan Hoffman <ryanhoffman4@gmail.com>
2026-08-28 09:04:17 -0700

    more router troubleshooting

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
 
 function App() {
 
   return (
-
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </>
+    <Home />
   );
-}
+};
 
 export default App
